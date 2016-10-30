07978d4fb3ef7591567767d68d5cadbea805de15
Ahmet Bilal Can <eybisii@gmail.com>
2016-10-30 20:42:09 +0200

    Added create,seed and test routes for the USER table

--- Auth.py
+++ Auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
 from flask import render_template
+from SQL_init import create_user_table
+from SQL_init import seed_user_table
+from SQL_init import test_user_table
 
 auth = Blueprint('auth', __name__)
 
@@ -7,3 +10,24 @@ auth = Blueprint('auth', __name__)
 @auth.route("/auth")
 def main():
     return render_template('login.html')
+
+@auth.route("/create-user-table")
+def create_table():
+    create_user_table()
+    return "user table created"
+
+@auth.route("/seed-user-table")
+def seed_table():
+    seed_user_table()
+    return "added sample seeds"
+
+@auth.route("/create-and-seed-user-table")
+def create_and_seed():
+    create_user_table()
+    seed_user_table()
+    return
+
+@auth.route("/test-user-table")
+def testdb():
+    count = test_user_table()
+    return "number of users in the user table: %d" % count
