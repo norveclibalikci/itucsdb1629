e5ca070c4491f4109291106543e872fe02584e31
Şahin Olut <sahinolut@outlook.com>
2016-10-31 00:07:37 +0200

    Fixed some issues regarding intendation

--- SQL_init.py
+++ SQL_init.py
@@ -203,11 +203,10 @@ def test_publication_table():
     
     
     
-    
-#Creating post table with three fields, post_id, profile_id, category_id
+ #Creating post table with three fields, post_id, profile_id, category_id
 def create_post_table():
 	with dbApi.connect(app.config['dsn']) as connection:
-	cursor = connection.cursor()
+		cursor = connection.cursor()
 
 	query = """DROP TABLE IF EXISTS POST"""
 	cursor.execute(query)
@@ -225,31 +224,29 @@ def create_post_table():
 #Seed the post table with 3 random values
 def seed_post_table():
 	with dbApi.connection(app.config['dsn']) as connection:
-		cursor = connection.cursor()
+			cursor = connection.cursor()
 
 
-		query= """INSERT INTO
-				POST (post_id, profile_id, category_id)
-				VALUES
-				(1,22,3),
-				(4,11,2),
-				(3,33,1)"""
-		cursor.execute(query)
-		connection.commit()
-		return True
+			query= """INSERT INTO
+					POST (post_id, profile_id, category_id)
+					VALUES
+					(1,22,3),
+					(4,11,2),
+					(3,33,1)"""
+			cursor.execute(query)
+			connection.commit()
+	return True
 #Testing post table with 3 random values
 def test_post_table():
 	with dbApi.connect(app.config['dsn']) as connection:
 		cursor= connection.cursor()
 
 		query= """SELECT COUNT(*) FROM POST;"""
-		
+
 		cursor.execute(query)
 
 		connection.commit()
 
 		count=cursor.fetchone()[0]
 		return count
-    
-    
     
