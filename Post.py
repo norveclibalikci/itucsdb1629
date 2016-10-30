6a48609edcafdc940b70719e8aa80b662b0e1469
Şahin Olut <sahinolut@outlook.com>
2016-10-31 00:08:05 +0200

    Update Post.py

--- Post.py
+++ Post.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 from flask import render_template
+from flask import redirect
+from SQL_init import create_post_table
+from SQL_init import seed_post_table
+from SQL_init import test_post_table
 
 post = Blueprint('post', __name__)
 
@@ -7,3 +11,25 @@ post = Blueprint('post', __name__)
 @post.route("/post")
 def main():
     return render_template("post.html")
+
+@post.route("/create-post-table")
+def create_table():
+	create_post_table()
+	return redirect('/')
+
+@post.route("/seed-post-table")
+def seed_table():
+	seed_post_table()
+	return redirect('/')
+
+@post.route("/create-and-seed-post-table")
+def create_and_seed():
+    create_post_table()
+    seed_post_table()
+    return redirect('/')
+
+
+@post.route("/test-post-table")
+def testdb():
+    count = test_post_table()
+    return "Record count: %d." % count
