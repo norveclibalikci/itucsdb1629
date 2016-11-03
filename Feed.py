2a8febe8174b8136846c5a5f03def15940c6c154
Burak Karakan <burak.karakan@boosmart.com>
2016-11-03 14:00:32 +0200

    Created all the CRUD methods and views.

--- Feed.py
+++ Feed.py
@@ -1,16 +1,24 @@
 from flask import Blueprint
 from flask import render_template
 from flask import redirect
+from flask import request
+
 from SQL_init import create_feed_table
 from SQL_init import seed_feed_table
 from SQL_init import test_feed_table
+from SQL_init import get_all_feed
+from SQL_init import create_feed_post
+from SQL_init import get_feed_post_with_id
+from SQL_init import update_feed_post
+from SQL_init import delete_feed_post
 
 feed = Blueprint('feed', __name__)
 
 
 @feed.route("/feed")
 def main():
-    return render_template('feed.html')
+    feed = get_all_feed()
+    return render_template('feed.html', feed=feed)
 
 
 @feed.route("/create-feed-table")
@@ -37,3 +45,34 @@ def testdb():
     count = test_feed_table()
     return "Number of records: %d." % count
 
+
+@feed.route("/create-feed-post", methods=['GET','POST'])
+def create_post_feed():
+    if request.method == "POST":
+        id = request.form.get('post_id')
+        title = request.form.get('post_title')
+        author = request.form.get('author')
+        likes = request.form.get('number_of_likes')
+        create_feed_post(id, title, author, likes)
+        return redirect('/feed')
+    return render_template("create_feed_post.html")
+
+
+@feed.route("/update-post/<post_id>", methods=['GET','POST'])
+def update_post_feed(post_id):
+    if request.method == "POST":
+        id = request.form.get('post_id')
+        title = request.form.get('post_title')
+        author = request.form.get('author')
+        likes = request.form.get('number_of_likes')
+        update_feed_post(id, title, author, likes)
+        return redirect('/feed')
+    else:
+        data = get_feed_post_with_id(post_id)
+        return render_template("update_feed_post.html", data=data)
+
+
+@feed.route("/delete-post/<post_id>")
+def delete_post_feed(post_id):
+    delete_feed_post(post_id)
+    return redirect('/feed')
