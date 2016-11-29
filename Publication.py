cdd61ad82542b7818131c1b0493957b26e4d261a
Uğur Uysal <uguruy3939@gmail.com>
2016-11-29 05:13:02 +0200

    For new table CRUD operations are done
    I changed old table respect to teacher's words at ninova.

--- Publication.py
+++ Publication.py
@@ -12,90 +12,144 @@ publication = Blueprint('publication', __name__)
 
 @publication.route("/publications")
 def main():
-   feed=get_all_feed()
-   return render_template('publications.html',feed = feed)
-
-@publication.route("/publications", methods=['POST'])
+   pubs=get_all_publications()
+   authors=get_all_authors()
+   return render_template('publications.html',pubs = pubs,authors=authors)
+  
+@publication.route("/publications/add", methods=['POST'])
 def new_publication_form():
-   title = request.form['title']
-   publisher = request.form['publisher']
-   id = request.form['pub_id']
-   
-   connection = dbApi.connect(app.config['dsn'])
-   with connection:
-        
+    authors  = request.form['author']
+    title = request.form['title']
+    publisher = request.form['publisher']
+
+    connection = dbApi.connect(app.config['dsn'])
+    with connection.cursor() as cur:
+        query = """SELECT author_id FROM AUTHORS 
+                    WHERE author_name = %s"""
+        cur.execute(query,(authors,))
+        try:
+            author_id = cur.fetchone()[0]
+        except:
+            insert_to_authors(authors)
+    connection.close()
+    connection2 = dbApi.connect(app.config['dsn'])
+    with connection2.cursor() as cur:
+        query = """SELECT author_id FROM AUTHORS 
+                    WHERE author_name = %s"""
+        cur.execute(query,(authors,)) 
+        author_id = cur.fetchone()[0]
         query = """INSERT INTO
-                PUBLICATION (publication_id,  publication_title, publisher)
+                PUBLICATION (publication_title, publisher, author_id)
                 VALUES
-                    (%s,%s, %s)"""
-        with connection.cursor() as cur:
-            cur.execute(query,(id,title,publisher))
-        
-   connection.close()
+                    (%s, %s, %s)"""
+        cur.execute(query,(title, publisher,author_id))  
+    connection2.commit()
+    connection2.close()
    
-   return redirect("/publications")
+    return redirect("/publications")
 
-
-@publication.route("/publications/delete" , methods=['POST'] )
-def delete_form():
-   de_id = request.form['del_id']
-   connection = dbApi.connect(app.config['dsn'])
-   with connection:
-        
-        query = """DELETE FROM PUBLICATION 
-                    WHERE publication_id = %s"""
-        with connection.cursor() as cur:
-            cur.execute(query,(de_id,))
-        
-   connection.close()
-   
-   return redirect("/publications")
-
-def get_all_feed():
+def insert_to_authors(author_name):
     with dbApi.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
+        query = """INSERT INTO AUTHORS (author_name) VALUES
+        (%s)"""
+        cursor.execute(query,(author_name,))
+        connection.commit()    
+    return
 
-        query = """SELECT * FROM PUBLICATION ORDER BY publication_id;"""
-        cursor.execute(query)
-        connection.commit()
-        return cursor
+@publication.route("/publications/delete" , methods=['POST'] )
+def delete_from_publication():
+   de_name = request.form['del_tit']
+   connection = dbApi.connect(app.config['dsn'])
+   with connection:        
+        query = """DELETE FROM PUBLICATION 
+                    WHERE publication_title = %s"""
+        with connection.cursor() as cur:
+            cur.execute(query,(de_name,))
+        
+   connection.close()
+   
+   return redirect("/publications")
 
 @publication.route("/publications/select" , methods=['POST'] )
-def select_from_form():
-   sel_id = request.form['sel_id'] 
+def select_from_publication():
+   aut_name = request.form['aut_name'] 
    with dbApi.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
 
-        query = """SELECT publication_title FROM PUBLICATION
-                    WHERE publication_id = %s"""
+        query = """SELECT COUNT(publication_id) FROM PUBLICATION,AUTHORS
+                    WHERE  author_name= %s AND PUBLICATION.author_id = AUTHORS.author_id"""
 
-        cursor.execute(query,(sel_id,))
+        cursor.execute(query,(aut_name,))
         connection.commit()
         x = cursor.fetchone()[0]
-        return x
+        return str(x)
 
 @publication.route("/publications/update" , methods=['POST'] )
-def update_form():
-   up_id = request.form['up_id']
+def update_publication():
+   up_name = request.form['up_name']
    up_tit = request.form['up_tit']
    with dbApi.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
 
         query = """UPDATE PUBLICATION SET publication_title = %s 
-                    WHERE publication_id =%s;"""
+                    WHERE publication_title =%s;"""
 
-        cursor.execute(query,(up_tit,up_id))
+        cursor.execute(query,(up_tit,up_name))
         connection.commit()
    return redirect("/publications")
 
-def get_all_feed():
+@publication.route('/publications/author-delete', methods=['POST'])
+def delete_from_authors():
+    aut_name = request.form['aut_name']
+    with dbApi.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """DELETE FROM AUTHORS WHERE author_name = %s"""
+
+        cursor.execute(query,(aut_name,))
+        connection.commit()
+    return redirect("/publications")
+
+@publication.route('/publications/author-add', methods=['POST'])
+def add_author():
+    aut_name = request.form['aut_name']
+    with dbApi.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """INSERT INTO AUTHORS (author_name)
+        VALUES (%s) """
+        cursor.execute(query,(aut_name,))
+        connection.commit()
+    return redirect("/publications")
+
+@publication.route('/publications/author-update', methods= ['POST'])
+def update_author():
+    up_name = request.form['up_name']
+    aut_name = request.form['aut_name']
+    with dbApi.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """UPDATE AUTHORS SET author_name = %s 
+                    WHERE author_name =%s;"""
+        cursor.execute(query,(aut_name,up_name))
+        connection.commit()
+    return redirect("/publications")
+
+def get_all_publications():
    with dbApi.connect(app.config['dsn']) as connection:
        cursor = connection.cursor()
-       query = """SELECT * FROM PUBLICATION;"""
+       query = """SELECT publication_id, publication_title, publisher, author_name FROM PUBLICATION,AUTHORS 
+                            WHERE PUBLICATION.author_id = AUTHORS.author_id"""
        cursor.execute(query)
        connection.commit()
        return cursor
 
+def get_all_authors():
+      with dbApi.connect(app.config['dsn']) as connection:
+       cursor = connection.cursor()
+       query = """SELECT * FROM AUTHORS"""
+       cursor.execute(query)
+       connection.commit()
+       return cursor
+   
 @publication.route("/create-publication-table")
 def create_table():
     var = create_publication_table()
@@ -125,4 +179,7 @@ def create_and_seed():
     create_publication_table()
     seed_publication_table()
     return redirect('/publications')
+
+
+
 
