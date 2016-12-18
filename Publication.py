9bb954e84b6b8871d3f28549e35794caebc292a1
Uğur Uysal <uguruy3939@gmail.com>
2016-12-18 23:00:14 +0200

    Update Publication.py

--- Publication.py
+++ Publication.py
@@ -10,7 +10,6 @@ from flask import current_app as app
 
 from flask_login import current_user
 from flask_login.utils import login_required
-
 import psycopg2 as dbApi
 publication = Blueprint('publication', __name__)
 
@@ -19,9 +18,7 @@ publication = Blueprint('publication', __name__)
 @login_required
 def main():
    pubs=get_all_publications()
-   authors=get_all_authors()
-   cat = get_all_cat()
-   return render_template('publications.html',pubs = pubs,authors=authors, cat = cat)
+   return render_template('publication/publications.html',pubs = pubs)
   
 @publication.route("/publications/add", methods=['POST'])
 @login_required
@@ -29,7 +26,7 @@ def new_publication_form():
     authors  = request.form['author']
     title = request.form['title']
     publisher = request.form['publisher']
-
+    category = request.form['category']
     connection = dbApi.connect(app.config['dsn'])
     with connection.cursor() as cur:
         query = """SELECT author_id FROM AUTHORS 
@@ -40,17 +37,33 @@ def new_publication_form():
         except:
             insert_to_authors(authors)
     connection.close()
+    
+    connection3 = dbApi.connect(app.config['dsn'])
+    with connection3.cursor() as cur:
+        query = """SELECT category_id FROM category 
+                    WHERE category_name = %s"""
+        cur.execute(query,(category,))
+        try:
+            category_id = cur.fetchone()[0]
+        except:
+            insert_category(category)
+    connection3.close()
+    
     connection2 = dbApi.connect(app.config['dsn'])
     with connection2.cursor() as cur:
         query = """SELECT author_id FROM AUTHORS 
                     WHERE author_name = %s"""
         cur.execute(query,(authors,)) 
         author_id = cur.fetchone()[0]
+        query = """SELECT category_id FROM category 
+                    WHERE category_name = %s"""
+        cur.execute(query,(category,)) 
+        category_id = cur.fetchone()[0]
         query = """INSERT INTO
-                PUBLICATION (publication_title, publisher, author_id)
+                PUBLICATION (publication_title, publisher, author_id, category_id)
                 VALUES
-                    (%s, %s, %s)"""
-        cur.execute(query,(title, publisher,author_id))  
+                    (%s, %s, %s, %s)"""
+        cur.execute(query,(title, publisher,author_id,category_id))  
     connection2.commit()
     connection2.close()
    
@@ -69,13 +82,13 @@ def insert_to_authors(author_name):
 @publication.route("/publications/delete" , methods=['POST'] )
 @login_required
 def delete_from_publication():
-   de_name = request.form['del_tit']
+   publication_id = request.form['publication_id']
    connection = dbApi.connect(app.config['dsn'])
    with connection:        
         query = """DELETE FROM PUBLICATION 
-                    WHERE publication_title = %s"""
+                    WHERE publication_id = %s"""
         with connection.cursor() as cur:
-            cur.execute(query,(de_name,))
+            cur.execute(query,(publication_id,))
         
    connection.close()
    
@@ -111,42 +124,6 @@ def update_publication():
         connection.commit()
    return redirect("/publications")
 
-@publication.route('/publications/author-delete', methods=['POST'])
-@login_required
-def delete_from_authors():
-    aut_name = request.form['aut_name']
-    with dbApi.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """DELETE FROM AUTHORS WHERE author_name = %s"""
-
-        cursor.execute(query,(aut_name,))
-        connection.commit()
-    return redirect("/publications")
-
-@publication.route('/publications/author-add', methods=['POST'])
-@login_required
-def add_author():
-    aut_name = request.form['aut_name']
-    with dbApi.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """INSERT INTO AUTHORS (author_name)
-        VALUES (%s) """
-        cursor.execute(query,(aut_name,))
-        connection.commit()
-    return redirect("/publications")
-
-@publication.route('/publications/author-update', methods= ['POST'])
-@login_required
-def update_author():
-    up_name = request.form['up_name']
-    aut_name = request.form['aut_name']
-    with dbApi.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """UPDATE AUTHORS SET author_name = %s 
-                    WHERE author_name =%s;"""
-        cursor.execute(query,(aut_name,up_name))
-        connection.commit()
-    return redirect("/publications")
 
 @login_required
 def get_all_publications():
@@ -154,15 +131,6 @@ def get_all_publications():
        cursor = connection.cursor()
        query = """SELECT publication_id, publication_title, publisher, author_name, category_name FROM PUBLICATION,AUTHORS,category
                             WHERE PUBLICATION.author_id = AUTHORS.author_id AND PUBLICATION.category_id = category.category_id """
-       cursor.execute(query)
-       connection.commit()
-       return cursor
-
-@login_required
-def get_all_authors():
-      with dbApi.connect(app.config['dsn']) as connection:
-       cursor = connection.cursor()
-       query = """SELECT * FROM AUTHORS"""
        cursor.execute(query)
        connection.commit()
        return cursor
@@ -202,10 +170,8 @@ def create_and_seed():
     seed_publication_table()
     return redirect('/publications')
 
-@publication.route("/publications/cat-add",  methods= ['POST'])
 @login_required
-def add_category():
-    category_name = request.form['cat_name']
+def insert_category(category_name):
     with dbApi.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
         query = """INSERT INTO category (category_name)
@@ -213,29 +179,7 @@ def add_category():
         cursor.execute(query,(category_name,))
         connection.commit()
         
-    return redirect("/publications")
-
-@publication.route("/publications/cat-del",  methods= ['POST'])
-@login_required
-def del_category():
-    category_name = request.form['cat_name']
-    with dbApi.connect(app.config['dsn']) as connection:
-        cursor = connection.cursor()
-        query = """DELETE FROM category WHERE category_name = %s  """
-        cursor.execute(query,(category_name,))
-        connection.commit()
-        
-    return redirect("/publications")
-
-
-@login_required
-def get_all_cat():
-      with dbApi.connect(app.config['dsn']) as connection:
-       cursor = connection.cursor()
-       query = """SELECT * FROM category"""
-       cursor.execute(query)
-       connection.commit()
-       return cursor
+    return
 
 
 
