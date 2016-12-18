f69cacc83623086c29a6742c9542f98381bbd2f8
Uğur Uysal <uguruy3939@gmail.com>
2016-12-18 15:20:55 +0200

    Update Publication.py

--- Publication.py
+++ Publication.py
@@ -2,21 +2,29 @@ from flask import Blueprint
 from flask import render_template
 from flask import redirect
 from flask import request
+
 from SQL_init import create_publication_table
 from SQL_init import seed_publication_table
 from SQL_init import test_publication_table
 from flask import current_app as app
+
+from flask_login import current_user
+from flask_login.utils import login_required
+
 import psycopg2 as dbApi
 publication = Blueprint('publication', __name__)
 
 
 @publication.route("/publications")
+@login_required
 def main():
    pubs=get_all_publications()
    authors=get_all_authors()
-   return render_template('publications.html',pubs = pubs,authors=authors)
+   cat = get_all_cat()
+   return render_template('publications.html',pubs = pubs,authors=authors, cat = cat)
   
 @publication.route("/publications/add", methods=['POST'])
+@login_required
 def new_publication_form():
     authors  = request.form['author']
     title = request.form['title']
@@ -48,6 +56,7 @@ def new_publication_form():
    
     return redirect("/publications")
 
+@login_required
 def insert_to_authors(author_name):
     with dbApi.connect(app.config['dsn']) as connection:
         cursor = connection.cursor()
@@ -58,6 +67,7 @@ def insert_to_authors(author_name):
     return
 
 @publication.route("/publications/delete" , methods=['POST'] )
+@login_required
 def delete_from_publication():
    de_name = request.form['del_tit']
    connection = dbApi.connect(app.config['dsn'])
@@ -72,6 +82,7 @@ def delete_from_publication():
    return redirect("/publications")
 
 @publication.route("/publications/select" , methods=['POST'] )
+@login_required
 def select_from_publication():
    aut_name = request.form['aut_name'] 
    with dbApi.connect(app.config['dsn']) as connection:
@@ -86,6 +97,7 @@ def select_from_publication():
         return str(x)
 
 @publication.route("/publications/update" , methods=['POST'] )
+@login_required
 def update_publication():
    up_name = request.form['up_name']
    up_tit = request.form['up_tit']
@@ -100,6 +112,7 @@ def update_publication():
    return redirect("/publications")
 
 @publication.route('/publications/author-delete', methods=['POST'])
+@login_required
 def delete_from_authors():
     aut_name = request.form['aut_name']
     with dbApi.connect(app.config['dsn']) as connection:
@@ -111,6 +124,7 @@ def delete_from_authors():
     return redirect("/publications")
 
 @publication.route('/publications/author-add', methods=['POST'])
+@login_required
 def add_author():
     aut_name = request.form['aut_name']
     with dbApi.connect(app.config['dsn']) as connection:
@@ -122,6 +136,7 @@ def add_author():
     return redirect("/publications")
 
 @publication.route('/publications/author-update', methods= ['POST'])
+@login_required
 def update_author():
     up_name = request.form['up_name']
     aut_name = request.form['aut_name']
@@ -133,15 +148,17 @@ def update_author():
         connection.commit()
     return redirect("/publications")
 
+@login_required
 def get_all_publications():
    with dbApi.connect(app.config['dsn']) as connection:
        cursor = connection.cursor()
-       query = """SELECT publication_id, publication_title, publisher, author_name FROM PUBLICATION,AUTHORS 
-                            WHERE PUBLICATION.author_id = AUTHORS.author_id"""
+       query = """SELECT publication_id, publication_title, publisher, author_name, category_name FROM PUBLICATION,AUTHORS,category
+                            WHERE PUBLICATION.author_id = AUTHORS.author_id AND PUBLICATION.category_id = category.category_id """
        cursor.execute(query)
        connection.commit()
        return cursor
 
+@login_required
 def get_all_authors():
       with dbApi.connect(app.config['dsn']) as connection:
        cursor = connection.cursor()
@@ -150,7 +167,9 @@ def get_all_authors():
        connection.commit()
        return cursor
    
+
 @publication.route("/create-publication-table")
+@login_required
 def create_table():
     var = create_publication_table()
     if var:
@@ -160,6 +179,7 @@ def create_table():
 
 
 @publication.route("/seed-publication-table")
+@login_required
 def seed_table():
     var = seed_publication_table()
     if var:
@@ -169,16 +189,53 @@ def seed_table():
 
 
 @publication.route("/test-publication-table")
+@login_required
 def test_table():
     count  = test_publication_table() 
     return "Number of records in the Publication table: %d." % count
    
  
 @publication.route("/create-and-seed-publication-table")
+@login_required
 def create_and_seed():
     create_publication_table()
     seed_publication_table()
     return redirect('/publications')
+
+@publication.route("/publications/cat-add",  methods= ['POST'])
+@login_required
+def add_category():
+    category_name = request.form['cat_name']
+    with dbApi.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """INSERT INTO category (category_name)
+        VALUES (%s) """
+        cursor.execute(query,(category_name,))
+        connection.commit()
+        
+    return redirect("/publications")
+
+@publication.route("/publications/cat-del",  methods= ['POST'])
+@login_required
+def del_category():
+    category_name = request.form['cat_name']
+    with dbApi.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        query = """DELETE FROM category WHERE category_name = %s  """
+        cursor.execute(query,(category_name,))
+        connection.commit()
+        
+    return redirect("/publications")
+
+
+@login_required
+def get_all_cat():
+      with dbApi.connect(app.config['dsn']) as connection:
+       cursor = connection.cursor()
+       query = """SELECT * FROM category"""
+       cursor.execute(query)
+       connection.commit()
+       return cursor
 
 
 
